511a22267a9e253a3593a2bde597b664c265ee3b
wujuno <86146780+wujuno@users.noreply.github.com>
2022-12-19 11:24:44 +0900

    Add resolvers

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const typeDefs =gql`
-    type Query {
-        text: String
-        hello: String
+let tweets = [
+    {
+        id:"1",
+        text:"first text"
+    },
+    {
+        id:"2",
+        text:"second text"
     }
+];
+
+
+const typeDefs =gql`
+    type User {
+        id: ID!
+        username: String!
+    }
+    type Tweet {
+        id: ID!
+        text: String!
+        author: User
+    }
+    type Query {
+        allTweets: [Tweet!]!
+        tweet(id:ID): Tweet
+    }
+    type Mutation {
+        postTweet(text:String!, userId: ID!): Tweet!
+        deleteTweet(id:ID!): Boolean!
+    } 
 `;
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+    Query: {
+        allTweets(){
+            return tweets;
+        },
+        tweet(root,{id}){
+            return tweets.find(tweet => tweet.id === id);
+        }
+    },
+    Mutation: {
+        postTweet(_,{text, userId}){
+            const newTweet = {
+                id: tweets.length +1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet
+        },
+        deleteTweet(_,{id}){
+            const tweet = tweets.find(tweet => tweet.id ===id);
+            if (!tweet) return false;
+            tweets = tweets.filter(tweet => tweet.id !==id);
+            return true;
+        }
+    }
+};
+
+
+const server = new ApolloServer({typeDefs,resolvers});
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
